4a8020454872247ae1796439233b6d19dac99962
Saban-dou <doumahadoumeda@gmail.com>
2026-06-23 15:09:04 +0300

    Auto-migrate au démarrage

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+# Lance les migrations automatiquement au démarrage
+from django.core.management import call_command
+try:
+    call_command('migrate', '--run-syncdb', verbosity=0)
+except Exception:
+    pass
 
 application = get_wsgi_application()
