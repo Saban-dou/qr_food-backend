404aeee4225342ffd509ade641317e17434a6999
Saban-dou <doumahadoumeda@gmail.com>
2026-06-23 15:12:57 +0300

    Auto-seed au démarrage

--- config/wsgi.py
+++ config/wsgi.py
@@ -3,10 +3,11 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-# Lance les migrations automatiquement au démarrage
 from django.core.management import call_command
 try:
     call_command('migrate', '--run-syncdb', verbosity=0)
+    call_command('seed_accueil', verbosity=0)
+    call_command('seed_menu', verbosity=0)
 except Exception:
     pass
 
